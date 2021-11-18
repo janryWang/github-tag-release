c2b5bef056001c29454e593843963ac0b37d5456
zhili.wzl <zhili.wzl@alibaba-inc.com>
2021-11-18 11:08:56 +0800

    chore: update code

--- src/index.ts
+++ src/index.ts
@@ -4,8 +4,11 @@ import { commit } from './commit'
 
 const runner = async () => {
   await createChangelogFile()
+  console.info('ChangeLog generated success!')
   await createReleaseNote()
+  console.info('ReleaseNote generated success!')
   await commit()
+  console.info('Git Commit Success!')
 }
 
 runner()
